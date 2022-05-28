b40b4b5ae3af88c6dbaa5f6e37778f15c0cc76a8
Katerina Tuckova <tuckova.mail@gmail.com>
2022-05-28 12:18:05 +0200

    Reseni 15. ulohy

--- src/components/UserName/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./style.css"
-
-export const UserName = () => {
-  return (
-    <p className="user-name">Neroxx</p>
-  )
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { JokeBody } from './components/JokeBody';
-import { JokeLikes } from './components/JokeLikes';
+import { Joke } from './components/Joke';
 import './style.css';
+import jokes from "./jokes"
 
 const App = () => (
   <div className="container">
-    <div className='joke'>
-      <JokeBody />
-      <JokeLikes />
-    </div>
+    {jokes.map((joke) =>
+      <Joke
+        key={joke.id}
+        userAvatar={joke.avatar}
+        userName={joke.name}
+        text={joke.text}
+        likes={joke.likes}
+        dislikes={joke.dislikes}
+      />
+    )}
   </div>
 );
 
